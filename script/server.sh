11e65ab98cdc99099d9735ea801f4ed248ddb812
Eva <66324759+khyvie@users.noreply.github.com>
2025-11-30 19:09:35 +0000

    Update server.sh

--- script/server.sh
+++ script/server.sh
@@ -42,42 +42,41 @@ while true; do
 
 	# Start Minecraft server
 
-	java \
-		-Xms3400M -Xmx3400M \
-		\
-		-Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true \
-		-XX:+UseG1GC \
-		-XX:+ParallelRefProcEnabled \
-		-XX:MaxGCPauseMillis=200 \
-		-XX:+UnlockExperimentalVMOptions \
-		-XX:+DisableExplicitGC \
-		-XX:+AlwaysPreTouch \
-		-XX:G1HeapWastePercent=5 \
-		-XX:G1MixedGCCountTarget=4 \
-		-XX:G1MixedGCLiveThresholdPercent=90 \
-		-XX:G1RSetUpdatingPauseTimePercent=5 \
-		-XX:SurvivorRatio=32 \
-		-XX:+PerfDisableSharedMem \
-		-XX:MaxTenuringThreshold=1 \
-		-XX:G1NewSizePercent=30 \
-		-XX:G1MaxNewSizePercent=40 \
-		-XX:G1HeapRegionSize=8M \
-		-XX:G1ReservePercent=20 \
-		-XX:InitiatingHeapOccupancyPercent=15 \
-		-Xss8M \
-		-XX:MaxDirectMemorySize=512M \
-		\
-		-XX:-UsePerfData \
-		-Dpaper.playerconnection.keepalive=60 \
-		-DIReallyKnowWhatIAmDoingISwear \
-		-Dminecraft.api.env=custom \
-    	-Dminecraft.api.auth.host=https://drasl.cutefox.club/auth \
-   		-Dminecraft.api.account.host=https://drasl.cutefox.club/account \
-    	-Dminecraft.api.profiles.host=https://drasl.cutefox.club/account \
-    	-Dminecraft.api.session.host=https://drasl.cutefox.club/session \
-    	-Dminecraft.api.services.host=https://drasl.cutefox.club/services \
-		\
-		-jar server.jar nogui
+java \
+    -Xms3400M -Xmx3400M \
+    -Dusing.aikars.flags=https://mcflags.emc.gs \
+    -Daikars.new.flags=true \
+    -XX:+UseG1GC \
+    -XX:+ParallelRefProcEnabled \
+    -XX:MaxGCPauseMillis=200 \
+    -XX:+UnlockExperimentalVMOptions \
+    -XX:+DisableExplicitGC \
+    -XX:+AlwaysPreTouch \
+    -XX:G1HeapWastePercent=5 \
+    -XX:G1MixedGCCountTarget=4 \
+    -XX:G1MixedGCLiveThresholdPercent=90 \
+    -XX:G1RSetUpdatingPauseTimePercent=5 \
+    -XX:SurvivorRatio=32 \
+    -XX:+PerfDisableSharedMem \
+    -XX:MaxTenuringThreshold=1 \
+    -XX:G1NewSizePercent=30 \
+    -XX:G1MaxNewSizePercent=40 \
+    -XX:G1HeapRegionSize=8M \
+    -XX:G1ReservePercent=20 \
+    -XX:InitiatingHeapOccupancyPercent=15 \
+    -Xss8M \
+    -XX:MaxDirectMemorySize=512M \
+    -XX:-UsePerfData \
+    -Dpaper.playerconnection.keepalive=60 \
+    -DIReallyKnowWhatIAmDoingISwear \
+    -Dminecraft.api.env=custom \
+    -Dminecraft.api.auth.host=https://drasl.cutefox.club/auth \
+    -Dminecraft.api.account.host=https://drasl.cutefox.club/account \
+    -Dminecraft.api.profiles.host=https://drasl.cutefox.club/account \
+    -Dminecraft.api.session.host=https://drasl.cutefox.club/session \
+    -Dminecraft.api.services.host=https://drasl.cutefox.club/services \
+    -jar server.jar nogui
+
 
 	# Stop alive checker (will be started again on the next run)
 
